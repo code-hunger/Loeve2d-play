533e72e631e77488653f204e0d718e1e61cc3db2
Code Hunger <beinghavingbreackfast@gmail.com>
2018-07-25 14:09:57 +0300

    Set the update() function of each ship individually.
    Decouples the world update from each ship's update, allowing for
    different updaters for every ship

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ end
 
 function love.update(dt)
   each(function (ship)
-    update_ship(dt, ship)
+    ship:update(dt)
   end, ships)
 end
 
@@ -21,7 +21,9 @@ end
 
 function love.keypressed(key)
   if key == "+" or key == "kp+" then
-    table.insert(ships, ship_factory:produce_ship())
+    ship = ship_factory:produce_ship()
+    ship.update = update_ship
+    table.insert(ships, ship)
   elseif key == "-" or key == "kp-" then
     table.remove(ships, 1)
   end
@@ -31,7 +33,7 @@ function draw_ship(ship)
   love.graphics.circle("line", ship.x, ship.y, 10)
 end
 
-function update_ship(t, ship)
+function update_ship(ship, t)
   ship.x = ship.x + t * 50 * math.cos(ship.angle)
   ship.y = ship.y + t * 50 * math.sin(ship.angle)
 end
