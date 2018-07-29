ec932d3fbd85ed9401f8960b6af7fb80792dffeb
Code Hunger <beinghavingbreackfast@gmail.com>
2018-07-29 14:15:39 +0300

    Make the aproaching to the circle of idle ships more smooth using deltaR
    Instead of a fixed angle per update for calculating the next target of a
    ship, base the angle on how close the ship is to its target radius.

--- pilots.lua
+++ pilots.lua
@@ -2,12 +2,16 @@ local pilots = {}
 
 local function angle2(x1, y1, x2, y2) return math.atan2(y2-y1, x2-x1) end
 
+local function pytag(x1, y1, x2, y2) return ((x2-x1) ^ 2 + (y2-y1) ^ 2) ^ 0.5 end
+
 function pilots.idle(x, y, idle_state, delta_time)
   local center_x = idle_state.center.x
   local center_y = idle_state.center.y
 
+  local delta_radius = pytag(center_x, center_y, x, y) - idle_state.radius
+
   local current_angle = angle2(center_x, center_y,  x, y)
-  local new_angle = current_angle + 10 * delta_time
+  local new_angle = current_angle + delta_time * (5 + 30 * math.abs(delta_radius / idle_state.radius))
 
   local target_x = center_x + idle_state.radius * math.cos(new_angle)
   local target_y = center_y + idle_state.radius * math.sin(new_angle)
