b459e9a509173f1e7dea89f4cb60dcbddc21559d
Code Hunger <beinghavingbreackfast@gmail.com>
2018-07-31 00:02:17 +0300

    Take formations out of main to their own module

--- main.lua
+++ main.lua
@@ -10,25 +10,11 @@ ship_factory.y = 400
 local ships
 
 local squadron = require "./squadron"
-
-function sine_formation(ships, i, leader)
-  return leader.x + 30 * i , leader.y+ 30 * math.sin(i * math . pi / 5)
-end
-
-function row_formation(ships, i, leader)
-  local previous
-  if i == 1 then previous = leader else previous = ships[i-1] end
-  return previous.x + 30 * math.cos(previous.angle), previous.y + 30 * math.sin(previous.angle)
-end
-
-function circle_formation(radius, arc, ships, i, leader)
-  local angle = i / #ships * arc + (math.pi * 2 - arc) /2
-  return leader.x + math.sin(angle) * radius, leader.y + math.cos(angle) * radius
-end
+local formations = require "./formations"
 
 function love.load()
   ships = {}
-  squadron:set_formation(row_formation)
+  squadron:set_formation(formations.row)
 end
 
 local next_ship = 0.5
