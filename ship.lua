33c269bd70e49622fe8dd9ba8ced0332c52f4edb
Code Hunger <beinghavingbreackfast@gmail.com>
2019-02-27 22:31:25 +0200

    Fix angle calculation with proper signs in both axes

--- ship.lua
+++ ship.lua
@@ -10,7 +10,7 @@ function Ship:draw(color)
   love.graphics.circle("line", l.x, l.y, 5)
 
   love.graphics.setColor(1, 1, 0)
-  love.graphics.line(l.x, l.y, l.x - math.cos(self.angle) * 10, l.y - math.sin(self.angle) * 10)
+  love.graphics.line(l.x, l.y, l.x + math.cos(self.angle) * 10, l.y + math.sin(self.angle) * 10)
 
   love.graphics.setColor(1,1,1)
   love.graphics.print("E = " .. utils.round(self.energy, 1), l.x - 20, l.y)
@@ -25,13 +25,13 @@ function Ship:draw(color)
     local r = self.energy / (self.initial_energy or 20) * math.pi * 2
     love.graphics.setLineWidth(1)
     love.graphics.setColor(0.4, 0.4, 0.4)
-    love.graphics.arc("line", l.x, l.y, (self.scan.radius or 20) * 0.8, math.pi + self.angle,r + self.angle + math.pi)
+    love.graphics.arc("line", l.x, l.y, (self.scan.radius or 20) * 0.8, self.angle,r + self.angle)
   end
 end
 
 function Ship:update(delta_time)
-  return self.location.x - delta_time * self.speed * math.cos(self.angle),
-         self.location.y - delta_time * self.speed * math.sin(self.angle)
+  return self.location.x + delta_time * self.speed * math.cos(self.angle),
+         self.location.y + delta_time * self.speed * math.sin(self.angle)
 end
 
 return Ship
